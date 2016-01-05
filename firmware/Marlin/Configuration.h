3cdad0cf6deb4d9ef8ae19f1afcbda5c92aa183f
Diego Trapero <me@diegotrap.com>
2016-01-05 17:27:12 +0100

    Reduced hotted maximum temperature
    Reduced heater 0 (hotend) maxtemp to comply with the recommended
    maximum temperature for the e3d lite6 hotted.

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -167,10 +167,10 @@ Here are some standard links for getting your machine calibrated:
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
 // You should use MINTEMP for thermistor short/failure protection.
-#define HEATER_0_MAXTEMP 250
-#define HEATER_1_MAXTEMP 250
-#define HEATER_2_MAXTEMP 250
-#define HEATER_3_MAXTEMP 250
+#define HEATER_0_MAXTEMP 245
+#define HEATER_1_MAXTEMP 245
+#define HEATER_2_MAXTEMP 245
+#define HEATER_3_MAXTEMP 245
 #define BED_MAXTEMP 150
 
 // If your bed has low resistance e.g. .6 ohm and throws the fuse you can duty cycle it to reduce the
