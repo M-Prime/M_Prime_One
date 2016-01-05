bff57f72f0035b799f77d6c29544bf66025e0c3c
Diego Trapero <me@diegotrap.com>
2016-01-05 18:03:02 +0100

    Restricted hotend maximum power
    Restricted the current to the hotend to 150 (max is 255), recommended
    setting for e3d lite6

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -189,7 +189,7 @@ Here are some standard links for getting your machine calibrated:
 
 // Comment the following line to disable PID and enable bang-bang.
 #define PIDTEMP
-#define BANG_MAX 255 // limits current to nozzle while in bang-bang mode; 255=full current
+#define BANG_MAX 150 // limits current to nozzle while in bang-bang mode; 255=full current
 #define PID_MAX BANG_MAX // limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #ifdef PIDTEMP
   //#define PID_DEBUG // Sends debug data to the serial port.
